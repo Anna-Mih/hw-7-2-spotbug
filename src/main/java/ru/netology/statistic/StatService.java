880c9c72fc0dfb3d0cb00aa7bfe0f4f8aab10345
anna-mih <anedbalskaya@yandex.ru>
2022-04-13 12:00:58 +0300

    modified StatService.java

--- src/main/java/ru/netology/statistic/StatService.java
+++ src/main/java/ru/netology/statistic/StatService.java
@@ -9,7 +9,6 @@ public class StatService {
      */
 
     public long findMax(long[] incomes) {
-        int[] mas = new int [7];
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
